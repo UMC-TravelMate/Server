9b9c54d4b75cb510e518767f3633560f493eb722
hig1002 <hig7463@naver.com>
2024-07-20 20:15:05 +0900

    init setting

---
+++ src/test/java/UMC/TravelMate/TravelMateApplicationTests.java
@@ -0,0 +1,13 @@
+package UMC.TravelMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
