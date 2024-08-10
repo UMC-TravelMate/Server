1233bbfa5cb679065c668ff603a9cc3d729689f2
hig1002 <129808885+hig1002@users.noreply.github.com>
2024-08-11 06:18:07 +0900

    Revert "Feat: MannerTemperature 관련 API 생성"

--- src/main/java/UMC/TravelMate/domain/MannerTemperature/repository/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package UMC.TravelMate.domain.MannerTemperature.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-    List<Review> findByPostId(Long postId);
-}
